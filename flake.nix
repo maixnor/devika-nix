83f96e6fdbf196c655f1c7fc65cac4bc3270d7b5
maixnor <46966993+maixnor@users.noreply.github.com>
2024-04-11 06:13:40 +0200

    direnv

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       defaultPackage = self.packages.devika;
 
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.bashInteractive ];
+        nativeBuildInputs = with pkgs; [ bashInteractive playwright-driver.browsers ];
         # ollama implied
         buildInputs = with pkgs; [
           python3
@@ -22,19 +22,25 @@
           playwright
           bun
           #self.packages.devika
+
+          # dev dependencies
+          dotnet-sdk_8
+          dotnet-aspnetcore_8
+          dotnet-runtime_8
+          dotnetPackages.SharpZipLib
         ];
         shellHook = ''
           cd devika/
           uv venv
           source .venv/bin/activate
           uv pip install -r requirements.txt
-          playwright install --with-deps
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
           cd ui/
           bun install
           bun run dev &
           cd ..
           python3 devika.py
-          exit
         '';
       };
     });
